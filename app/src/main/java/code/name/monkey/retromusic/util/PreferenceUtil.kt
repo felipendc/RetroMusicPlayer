fdcfbd2f950111b450398a87b7dc8faa199044dc
vicyoslife <felipendc10phoenix@gmail.com>
2025-01-28 20:16:00 -0300

    set the Album and Artists Grid size to 3 and Grid style to Normal

--- app/src/main/java/code/name/monkey/retromusic/util/PreferenceUtil.kt
+++ app/src/main/java/code/name/monkey/retromusic/util/PreferenceUtil.kt
@@ -398,7 +398,7 @@ object PreferenceUtil {
 
     var artistGridStyle: GridStyle
         get() {
-            val id: Int = sharedPreferences.getInt(ARTIST_GRID_STYLE, 3)
+            val id: Int = sharedPreferences.getInt(ARTIST_GRID_STYLE, 0)
             return GridStyle.values().firstOrNull { gridStyle ->
                 gridStyle.id == id
             } ?: GridStyle.Circular
